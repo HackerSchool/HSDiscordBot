3f3abd8b7844d1b646fdda59fb805d146ed879b7
ArmindoFlores <francisco.rodrigues0908@gmail.com>
2021-03-31 19:03:34 +0100

    Fixed a bug

--- src/utils.py
+++ src/utils.py
@@ -16,12 +16,16 @@ async def command_deletable(self, message, args):
 async def reaction_deletable(self, reaction, user, panel):
     """Triggered when a deletable message is reacted on"""
     if reaction.emoji == DELETE:
+        if "on_delete" in panel["info"]:
+            await panel["on_delete"](self, reaction, user, panel)
         self.remove_active_panel(reaction.message, panel["user"])
         await reaction.message.delete()
         
 async def reaction_yesno(self, reaction, user, panel):
     """Triggered when a yes-no message is reacted on"""
     if reaction.emoji == DELETE:
+        if "on_delete" in panel["info"]:
+            await panel["info"]["on_delete"](self, reaction, user, panel)
         self.remove_active_panel(reaction.message, panel["user"])
         await reaction.message.delete()
     
