fb93756d10306b72d48e437c76b1691dd76ac49e
Pedro Taborda <33559524+PedroTaborda@users.noreply.github.com>
2021-07-20 16:44:08 +0100

    added presence detection over a period of time

--- src/cmdpresent.py
+++ src/cmdpresent.py
@@ -1,6 +1,29 @@
 from logging import ERROR
 import discord
 from utils import WARNING_COLOR, SUCCESS_COLOR, ERROR_COLOR
+import datetime
+import logging
+
+# a user which is continuously present in least this % of the event is guaranteed to be marked as present
+minimum_sufficient_attendance_percentage = 40
+
+class PresenceMarker:
+    def __init__(self, guild, channel):
+        self.guild = guild
+        self.channel = channel
+        self.attendees = []
+    
+    async def mark_presence(self, dummy):
+        cur_active_members = members_in_vc(self.guild)
+        if len(cur_active_members) == 0:
+            return
+        for member in cur_active_members:
+            if member in self.attendees:
+                cur_active_members.remove(member)
+        self.attendees.extend(cur_active_members)
+    
+    async def deliver_attendance(self, dummy):
+        await _deliver_attendance(self.attendees, self.channel)
 
 def members_in_vc(guild : discord.Guild):
     all_members = guild.members
@@ -10,24 +33,54 @@ def members_in_vc(guild : discord.Guild):
             members.append(member)
     return members
 
+async def _deliver_attendance(members, channel):
+    names = [member.display_name for member in members ]
+
+    if len(names) == 0:
+        embed = discord.Embed(title="Marked attendance of zero users.", color=ERROR_COLOR)
+        embed.description = "Looks like no one showed up."
+    elif len(names) == 1:
+        embed = discord.Embed(title=f"Marked attendance of one users.", color=SUCCESS_COLOR)
+        embed.description = "Attendee:\n" + ", ".join(names)
+    else:
+        embed = discord.Embed(title=f"Marked attendance of {len(names)} users.", color=SUCCESS_COLOR)
+        embed.description = "Attendees:\n" + ", ".join(names)
+
+    await channel.send(embed=embed)
 
 async def command_present(self, message, args):
     user_input = True
     if '-y' in args:
         user_input = False
 
-    present = members_in_vc(message.channel.guild)
+    if len(args) == 0:
+        present = members_in_vc(message.channel.guild)
+        await _deliver_attendance(present, message.channel)
+        return
+    
+    if len(args) == 1:
+        try:
+            duration_minutes = float(args[0])
+            now = datetime.datetime.now()
+            
+            duration_seconds = duration_minutes * 60
+            check_intervals = duration_seconds*minimum_sufficient_attendance_percentage/100
 
-    names = [member.display_name for member in present ]
+            attendance_checker = PresenceMarker(message.guild, message.channel)
+            
+            mark_attendance_time = 0
+            while mark_attendance_time < duration_seconds:
+                t1 = now + datetime.timedelta(seconds = mark_attendance_time)
+                self.schedule(t1, t1, attendance_checker.mark_presence)
+                mark_attendance_time += check_intervals
+            t2 = now + datetime.timedelta(seconds = duration_seconds)
+            self.schedule(t2, t2, attendance_checker.deliver_attendance)
 
-    if len(names) == 0:
-        embed = discord.Embed(title="There are no users in voice chat at the moment.", color=ERROR_COLOR)
-        embed.description = "Looks like no one showed up yet."
-    elif len(names) == 1:
-        embed = discord.Embed(title=f"There is 1 user in voice chat at the moment.", color=SUCCESS_COLOR)
-        embed.description = "Attendee:\n" + ", ".join(names)
-    else:
-        embed = discord.Embed(title=f"There are {len(names)} users in voice chat at the moment.", color=SUCCESS_COLOR)
-        embed.description = "Attendees:\n" + ", ".join(names)
+            return
+        except NameError:
+            logging.log(NameError)
+            pass
 
-    await message.channel.send(embed=embed)
+    
+    embed = discord.Embed(title="Improper command usage!", color=ERROR_COLOR)
+    embed.description = f"Arguments '{args}' are invalid."
